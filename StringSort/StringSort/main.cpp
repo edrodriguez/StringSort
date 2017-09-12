ede5300c92e9bbd0dfb95c245ec97b7216595ed3
ed.roma19 <ed.roma19@gmail.com>
2017-09-12 09:48:57 -0400

    Improve in timing
    The timing is done on the main and not on the threads so it is closer to actual running time

--- StringSort/StringSort/main.cpp
+++ StringSort/StringSort/main.cpp
@@ -31,9 +31,9 @@ bool isFirstWordSmaller(string word1, string word2) {
 
 	//check the letters up to the common number of letters
 	for (int i = 0; i < smallerSize; ++i) {
-		if (word1[i] < word2[i])
+		if (word1[i] - word2[i] < 0)
 			return true;
-		else if (word1[i] > word2[i])
+		else if (word1[i] - word2[i] > 0)
 			return false;
 	}
 
@@ -96,7 +96,8 @@ void merge(vector<string> *words, int startPosition, int midPoint,
 }
 
 //recursive implementation of merge sort
-void mergeSort(vector<string> *words, int startPosition, int endPosition, int *numComparissons) {
+void mergeSort(vector<string> *words, int startPosition,
+	int endPosition, int *numComparissons) {
 
 	if (startPosition < endPosition) {
 		int midPoint = (startPosition + endPosition) / 2;
@@ -117,7 +118,8 @@ void writeResults(vector<string> *sortedWords) {
 	//you make less comparissons of variable count if you print the 50 first words
 	//and then you iterate again to write to the file
 	cout << "First 50 sorted words:" << endl;
-	for (vector<string>::const_iterator it = sortedWords->begin(); it != sortedWords->end(); ++it) {
+	for (vector<string>::const_iterator it = sortedWords->begin();
+		 it != sortedWords->end(); ++it) {
 		if (count < 50) {
 			cout << *it << endl;
 			++count;
@@ -128,7 +130,8 @@ void writeResults(vector<string> *sortedWords) {
 
 	outputFile.open("SortedWords.txt");
 	if (outputFile.good()) {
-		for (vector<string>::const_iterator it = sortedWords->begin(); it != sortedWords->end(); ++it) {
+		for (vector<string>::const_iterator it = sortedWords->begin();
+			 it != sortedWords->end(); ++it) {
 			outputFile << *it << endl;
 		}
 	}
@@ -136,53 +139,50 @@ void writeResults(vector<string> *sortedWords) {
 }
 
 //merges the sorted vectors from the different threads
-void finalMerge(vector<string> *firstWords, vector<string> *secondWords,
-	vector<string> *sortedWords, int *numComparissons, double *elapsedTime) {
+void mergeVectors(vector<string> *firstWords, vector<string> *secondWords,
+	vector<string> *sortedWords, int *numComparissons,
+	int *it1, int *it2) {
 
-	time_t startTime, endTime;
-	vector<string>::const_iterator it1 = firstWords->begin();
-	vector<string>::const_iterator it2 = secondWords->begin();
-
-	time(&startTime);
-	while (it1 != firstWords->end() && it2 != secondWords->end()) {
-		if (isFirstWordSmaller(*it1, *it2)) { //first word goes first
-			sortedWords->push_back(*it1);
-			++it1;
+	while (*it1 < firstWords->size() && *it2 < secondWords->size()) {
+		if (isFirstWordSmaller((*firstWords)[*it1], (*secondWords)[*it2])) { //first word goes first
+			sortedWords->push_back((*firstWords)[*it1]);
+			++(*it1);
 		}
 		else {
-			sortedWords->push_back(*it2);
-			++it2;
+			sortedWords->push_back((*secondWords)[*it2]);
+			++(*it2);
 		}
 		++(*numComparissons);
 	}
-	time(&endTime);
-	*elapsedTime = difftime(endTime, startTime);
+}
 
-	//copy remaining of first vector
-	while (it1 != firstWords->end()) {
-		sortedWords->push_back(*it1);
-		++it1;
-	}
+//copies words from one vector to another starting at the indicated
+//position
+void copyRemainingWords(vector<string> *words, vector<string> *sortedWords,
+	int it) {
 
-	//copy remaining of second vector
-	while (it2 != secondWords->end()) {
-		sortedWords->push_back(*it2);
-		++it2;
+	while (it < words->size()) {
+		sortedWords->push_back((*words)[it]);
+		++it;
 	}
 }
 
 int main() {	
 	ifstream inputFile;
-	vector<string> Q1Words, Q2Words, Q3Words, Q4Words, firstHalfWords, secondHalfWords, sortedWords;
+	vector<string> Q1Words, Q2Words, Q3Words, Q4Words,
+		firstHalfWords, secondHalfWords, sortedWords;
 	time_t startTime, endTime;
-	double firstHalfSeconds, secondHalfSeconds, totalSeconds, quarterSeconds;
-	int Q1Comparissons, Q2Comparissons, Q3Comparissons, Q4Comparissons, firstHalfComparissons, secondHalfComparissons, totalComparissons;
+	double totalSeconds, halvesSeconds, quarterSeconds;
+	int Q1Comparissons, Q2Comparissons, Q3Comparissons, Q4Comparissons,
+		firstHalfComparissons, secondHalfComparissons, totalComparissons;
+
 	Q1Comparissons = 0;
 	Q2Comparissons = 0;
 	Q3Comparissons = 0;
 	Q4Comparissons = 0;
 	firstHalfComparissons = 0;
 	secondHalfComparissons = 0;
+	totalComparissons = 0;
 
 	inputFile.open("MillionWords.txt");
 
@@ -192,7 +192,7 @@ int main() {
 		int fileLength = inputFile.tellg();
 		inputFile.seekg(0, inputFile.beg);
 
-		//read file
+		//read file in 4 parts
 		readFile(inputFile, fileLength / 4, &Q1Words);
 		readFile(inputFile, fileLength / 2, &Q2Words);
 		readFile(inputFile, 3 * fileLength / 4, &Q3Words);
@@ -215,20 +215,47 @@ int main() {
 		quarterSeconds = difftime(endTime, startTime);
 
 		//merge quarters into halves
-		thread H1 (finalMerge,&Q1Words, &Q2Words, &firstHalfWords, &firstHalfComparissons, &firstHalfSeconds);
-		thread H2(finalMerge, &Q3Words, &Q4Words, &secondHalfWords, &secondHalfComparissons, &secondHalfSeconds);
+		int it1 = 0;
+		int it2 = 0;
+		int it3 = 0;
+		int it4 = 0;
+		time(&startTime);
+		thread H1(mergeVectors, &Q1Words, &Q2Words, &firstHalfWords, &firstHalfComparissons, &it1, &it2);
+		thread H2(mergeVectors, &Q3Words, &Q4Words, &secondHalfWords, &secondHalfComparissons, &it3, &it4);
 
 		// synchronize threads
 		H1.join();
 		H2.join();
+		time(&endTime);
+		halvesSeconds = difftime(endTime, startTime);
+
+		//copy remaining words
+		copyRemainingWords(&Q1Words, &firstHalfWords, it1);
+		copyRemainingWords(&Q2Words, &firstHalfWords, it2);
+		copyRemainingWords(&Q3Words, &secondHalfWords, it3);
+		copyRemainingWords(&Q4Words, &secondHalfWords, it4);
 
 		//merge last 2 halves
-		finalMerge(&firstHalfWords, &secondHalfWords, &sortedWords, &totalComparissons, &totalSeconds);
+		it1 = 0;
+		it2 = 0;
+		time(&startTime);
+		mergeVectors(&firstHalfWords, &secondHalfWords, &sortedWords,
+			&totalComparissons, &it1, &it2);
+		time(&endTime);
+		totalSeconds = difftime(endTime, startTime);
 
-		totalSeconds = totalSeconds + quarterSeconds + firstHalfSeconds + secondHalfSeconds;
-		totalComparissons = totalComparissons + Q1Comparissons + Q2Comparissons + Q3Comparissons + Q4Comparissons + firstHalfComparissons + secondHalfComparissons;
+		//copy remaining words
+		copyRemainingWords(&firstHalfWords, &sortedWords, it1);
+		copyRemainingWords(&secondHalfWords, &sortedWords, it2);
+
+		totalComparissons = totalComparissons + Q1Comparissons + Q2Comparissons
+			+ Q3Comparissons + Q4Comparissons + firstHalfComparissons + secondHalfComparissons;
 		cout << "The number of comparissons used was " << totalComparissons << endl;
-		cout << "The total time elapsed is " << totalSeconds << " seconds." << endl;
+
+		totalSeconds = totalSeconds + quarterSeconds + halvesSeconds;
+		cout << "The total time elapsed for sorting is " << int(totalSeconds / 60);
+		cout << " minutes and " << int(totalSeconds) % 60 << " seconds." << endl;
+
 		writeResults(&sortedWords);
 	}
 
